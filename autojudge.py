b2f47322b73de42366d5d65f830fa2ccd27d7247
Stepan Blinov <blinov.school@yandex.ru>
2025-11-25 07:01:54 +0300

    Config changed
    Config file moved from given file dir to user config dir. Also now it gets some more data from the server (like language)

--- autojudge.py
+++ autojudge.py
@@ -7,25 +7,31 @@
 
 import requests, os, json, argparse
 
-CONFIG_FILENAME = "autojudge.json"
+CONFIG_FILENAME = os.path.expanduser(os.path.sep.join(["~", ".config", "autojudge", "settings.json"]))
 
 
 class Data:
-    def __init__(self, token: str, contest: str) -> None:
+    def __init__(self, token: str, contest: str, dir: str | None, data=None) -> None:
         self.token = token
         self.contest = contest
+        self.data = {"token": self.token, "contests": {dir: self.contest}} if data is None else data
         self.connection = Connection(self.token)
-        self.problems = self.connection.get("contest-status-json", contest_id=self.contest)["problems"]
+        result = self.connection.get("contest-status-json", contest_id=self.contest)
+        self.problems = result["problems"]
+        self.lang_name = result["compilers"][0]["short_name"]
+        self.sfx = result["compilers"][0]["src_sfx"]
     
 
     @classmethod
-    def read(cls, filename: str):
+    def read(cls, filename: str, dir: str):
         data = json.load(open(filename, 'rt', encoding="utf-8"))
-        return cls(data["token"], data["contest"])
+        if dir not in data["contests"]:
+            data["contests"][dir] = input("Input contest id for this directory: ")
+        return cls(data["token"], data["contests"][dir], None, data)
     
 
     def write(self, filename: str) -> None:
-        json.dump({"token": self.token, "contest": self.contest}, open(filename, "wt", encoding="utf-8"))
+        json.dump(self.data, open(filename, "wt", encoding="utf-8"))
     
 
     def send_problem(self, prob_id: str, file) -> str:
@@ -38,7 +44,7 @@ class Data:
                 raise KeyError(prob_id)
         else:
             id = prob_id
-        result = self.connection.post("submit-run", {"prob_id": id, "lang_id": "python3"}, {"file": file}, contest_id=self.contest)
+        result = self.connection.post("submit-run", {"prob_id": id, "lang_id": self.lang_name}, {"file": file}, contest_id=self.contest)
         return result["run_id"]
     
 
@@ -80,15 +86,14 @@ def main() -> None:
     parser = argparse.ArgumentParser()
     parser.add_argument("file", help="file to send", type=argparse.FileType(encoding="utf-8"))
     args = parser.parse_args()
-    config_path = os.path.join(os.path.dirname(args.file.name), CONFIG_FILENAME)
-    if not os.path.exists(config_path):
+    if not os.path.exists(CONFIG_FILENAME):
         token = input("Input your API token: ")
         contest = input("Input contest id for this dir: ")
-        data = Data(token, contest)
-        data.write(config_path)
-    data = Data.read(config_path)
+        data = Data(token, contest, os.path.dirname(args.file.name))
+        data.write(CONFIG_FILENAME)
+    data = Data.read(CONFIG_FILENAME, os.path.dirname(args.file.name))
     try:
-        run_id = data.send_problem(os.path.basename(args.file.name).removesuffix(".py"), args.file)
+        run_id = data.send_problem(os.path.basename(args.file.name).removesuffix(data.sfx), args.file)
     except KeyError:
         print("Avalible problems:", ", ".join([problem["short_name"] for problem in data.problems]))
         prob_name = input("Choose problem from listed above: ")
